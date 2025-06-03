729e975962a3697ce9e8dbbcadc697106ab285ec
Chan Wai Lung <chan0629w@gmail.com>
2025-06-03 18:37:48 +0800

    api installed

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,2 +1,6 @@
+// Description:
+// Main usage: This component fetches and stores the data from the API.
+// further description will be added later.
+
 import React, {useState, useEffect} from 'react';
 
